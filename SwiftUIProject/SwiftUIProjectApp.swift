8e63bdb437681ff218bee65cefb31fdd794cb9e9
Hyunndy <yoosa3004@naver.com>
2023-03-03 02:05:11 +0900

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by hyunndy on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
